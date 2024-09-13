dd6ac5926c2598c303f073fbe93525b61be0f92d
Trinkatdot <Akenathoncicero20@>
2024-09-13 11:52:13 -0300

    feat: ex3

--- funcoes_js/ex3/anotação.js
+++ funcoes_js/ex3/anotação.js
@@ -1,48 +1,26 @@
-/*Crie um algoritmo que leia um vetor de 10 letras, e diga quantas 
-consoantes foram lidas e mostre essas consoantes.
-*/
-let letras = [];
-let vogal = [];
-let cont = 0;
-let mensagem = "";
-for (let i = 0; i < 10; i += 1) {
-    let val = prompt("informe uma letra");
-    letras[i] = val;
-}
-for (j = 0; j < 10; j++) {
-    let val = letras[j].toUpperCase();
-    if (!(val == "A" || val == "E" || val == "I" || val == "O" || val == "U")) {
-        cont ++;
-        mensagem += val + "\n"
+function verificarVogais() {
+    let letras = [];
+    let contadorVogais = 0;
+    let consoantes = [];
+
+    // Leitura de 10 letras no vetor de letras
+    for (let i = 0; i < 10; i++) {
+        let letra = prompt("Informe uma Letra: ");
+        letras.push(letra);
     }
-} alert(mensagem + cont + " consoantes");
 
-//
-
-let letras = [];
-let contador = 0;
-let mensagem = "";
-
-//leitura de uma letra no vetor de letras
-for (let i = 0; i < 10; i++) {
-    let letra = prompt("Informe uma Letra: ");
-    letras.push(letra);
-}
-
-for (let j = 0; j < letras.length; j++) {
-    //converto o texto pra maiúsculo
-    let letra = letras[j].toUpperCase();
-    //Verifico se tem alguma vogal
-    let temA = (letra == "A"); // true ou false
-    let temE = (letra == "E"); // true ou false
-    let temI = (letra == "I"); // true ou false
-    let temO = (letra == "O"); // true ou false
-    let temU = (letra == "U"); // true ou false
-    
-    //se tiver alguma vogal ele não entra no if
-    if ((temA || temE || temI || temO || temU) == false) {
-        contador += 1;
-        mensagem += letra + "\n";
+    // Verificação de vogais e consoantes
+    for (let j = 0; j < letras.length; j++) {
+        let letra = letras[j].toUpperCase();
+        if (["A", "E", "I", "O", "U"].includes(letra)) {
+            contadorVogais++;
+        } else {
+            consoantes.push(letra);
+        }
     }
+
+    alert("Foram lidas " + contadorVogais + " vogais!");
+    alert("Consoantes: " + consoantes.join(", "));
 }
-alert(mensagem + "\n" + "Teve " + contador + " Consoantes!");
+
+verificarVogais();
--- funcoes_js/ex3/script.js
+++ funcoes_js/ex3/script.js
@@ -1,34 +1,34 @@
 /* Crie um algoritmo usando funções que faça uma leitura de 10 letras em um 
 vetor, e após verificar quantas vogais foram lidas e mostre somente 
 consoantes no alerta.*/
-function verificarvogais(){
+
+/*includes determina se um conjunto de caracteres pode ser 
+encontrado dentro de outra string*/
+
+function verificarvogais() {
     let letras = [];
-let contador = 0;
-let mensagem = "";
+    let vogais = 0;
+    let consoantes = 0;
 
-//leitura de uma letra no vetor de letras
-for (let i = 0; i < 5; i++) {
-    let letra = prompt("Informe uma Letra: ");
-    letras.push(letra);
-}
-
-for (let j = 0; j < letras.length; j++) {
-    //converto o texto pra maiúsculo
-    let letra = letras[j].toUpperCase();
-    //Verifico se tem alguma vogal
-    let temA = (letra == "A"); // true ou false
-    let temE = (letra == "E"); // true ou false
-    let temI = (letra == "I"); // true ou false
-    let temO = (letra == "O"); // true ou false
-    let temU = (letra == "U"); // true ou false
-    
-    //se tiver alguma vogal ele não entra no if
-    if ((temA || temE || temI || temO || temU) == true) {
-        contador += 1;
-        mensagem += letra + "\n";
+    //leitura de letra no vetor de letras
+    for (let i = 0; i < 5; i++) {
+        let letra = prompt("Informe uma Letra: ");
+        letras.push(letra);
     }
-}
-alert(mensagem + "\n" + "Teve " + contador + " vogais!");
+
+    for (let j = 0; j < letras.length; j++) {
+        //converto o texto pra maiúsculo
+        let letra = letras[j].toUpperCase();
+        //Verifico se tem alguma vogal
+        if (["A", "E", "I", "O", "U"].includes(letra)) {
+            vogais++;
+        } else {
+            consoantes.push(letra);
+        }
+    }
+    alert("Foram lidas " + Vogais + " vogais! e Consoantes: " + consoantes.join(", "));
+    
 }
 
 verificarvogais();
+
