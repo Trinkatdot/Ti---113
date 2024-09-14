01e288f9e0c6d9884ac7245a33176b17f0337b4d
Trinkatdot <Akenathoncicero20@>
2024-09-14 10:24:14 -0300

    feat: ex4 finalizado

--- funcoes_js/ex4/script.js
+++ funcoes_js/ex4/script.js
@@ -13,22 +13,39 @@ function cadastro() {
     let salarioBruto = [];
     let INSS = 0.9;
     //captura nome e slario até 50 pessoas
-    for (let i = 0; i <= 5; i++) {
+    while (continuar && pessoas.length < 50) {
+
         //captura o nome
         let nome = prompt("Informe o nome: ");
+        if (nome.trim() === "") {
+            alert("Nome não pode estar em branco. Tente novamente.");
+            continue;
+        }
+        //guarda nome
         pessoas.push(nome);
+
         //captura o salario e converte (número decimal)
-        let salarioBruto = parseFloat(prompt("Informe o salário bruto:"));
-        salarioBruto.push(salarioBruto);
+        let salario = parseFloat(prompt("Informe o salário bruto:"));
+        if (isNaN(salario) || salario < 0) {
+            alert("Salário inválido. Insira um número positivo.");
+            pessoas.pop(); // Remove o nome se o salário for invlido
+            continue;
+        }
+        //guarda o bruto
+        salarioBruto.push(salario);
+
         //pergunta se deseja continuar cadastrando
-        continuar = confirm("Deseja cadastrar outra pessoa? OK = para continuar cadastrando CANCELAR = Para parar de cadastrar")
+        continuar = confirm("Deseja cadastrar outra pessoa? OK = para continuar cadastrando ou  CANCELAR = Para parar de cadastrar")
         if (!continuar) {
             break;
         }
     }
     let mensagem = "Cadastro finalizado. Dados das pessoas cadastradas:\n";
-    let salarioLiquido = salarioBruto * 0.9;
-    mensagem += `Nome: ${pessoas}, Salário Bruto: R$${salarioBruto.toFixed(2)}, Salário Líquido: R$${salarioLiquido.toFixed(2)}\n`;
+    for (let i = 0; i < pessoas.length; i++) {
+        let salarioLiquido = salarioBruto[i] * INSS; //10% INSS
+        mensagem += `Nome: ${pessoas[i]}, Salário Bruto: R$${salarioBruto[i].toFixed(2)}, Salário Líquido: R$${salarioLiquido.toFixed(2)}\n`;
+    }
+
     alert(mensagem);
 }
 
